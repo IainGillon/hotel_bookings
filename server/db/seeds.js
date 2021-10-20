910a28c64be17aae0538ae511e1aecbb8542fd47
Iain Gillon <iaingillon@Iains-MacBook-Air.local>
2021-10-20 11:09:42 +0100

    2

---
+++ server/db/seeds.js
@@ -0,0 +1,44 @@
+use hotel_bookings;
+db.dropDatabase();
+db.games.insertMany([
+    {
+        name: "name 1",
+        checked_in: True,
+        email: "email@email.com"
+
+
+    },
+    {
+
+        name: "name 2",
+        checked_in: True,
+        email: "email2@email.com"
+
+    }
+  {
+
+        name: "name 3",
+        checked_in: False,
+        email: "email3@email.com"
+
+    }
+]);
+
+
+Message Vinnie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
